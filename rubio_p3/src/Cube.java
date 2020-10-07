31a7a1fc384386deb5e0aa9973817a363128cc06
Nestor Rubio <nestordrubio9@gmail.com>
2020-10-07 10:07:53 -0400

    added cube

---
+++ rubio_p3/src/Cube.java
@@ -0,0 +1,32 @@
+public class Cube extends Shape3D {
+    private double side_length;
+    public Cube(double side){
+        this.side_length = side;
+        this.name = "cube";
+        this.area = calculateArea();
+        this.volume = calculateVolume();
+    }
+    @Override
+    public double getVolume() {
+        return this.volume;
+    }
+
+    @Override
+    protected double calculateVolume() {
+        return Math.pow(side_length, 3);
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public double getArea() {
+        return this.area;
+    }
+    @Override
+    protected double calculateArea(){
+        return (6 * Math.pow(side_length, 2));
+    }
+}
