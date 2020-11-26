b466c1aabdebd91634423e378280cbea5fb87639
Nestor Rubio <nestordrubio9@gmail.com>
2020-11-26 09:29:32 -0500

    few more tests and corrected test to contain expected inputs

--- rubio_p5/src/ContactItemTest.java
+++ rubio_p5/src/ContactItemTest.java
@@ -10,21 +10,36 @@ class ContactItemTest {
         });
     }
     @Test
+    public void creationSucceedsbutwithBlankPhoneNumberandEmail(){
+        ContactItem c = new ContactItem("Me", "Way", "3523523523", "notgonnaprovideone");
+        assertEquals("Me Way: : ", c.toString());
+    }
+    @Test
+    public void creationSkipsPhoneifWrongFormat(){
+        ContactItem c = new ContactItem("First", "Last", "3216549870", "x@y.z");
+        assertEquals("", c.getPhonenumber());
+    }
+    @Test
+    public void creationSkipsEmailifWrongFormat(){
+        ContactItem c = new ContactItem("First", "Last", "321-654-9870", "x@y");
+        assertEquals("", c.getEmail());
+    }
+    @Test
     public void creationSucceedsWithBlankEmail(){
         assertDoesNotThrow(()->{
-            ContactItem c = new ContactItem("Nestor","Rubio","0123456789","");
+            ContactItem c = new ContactItem("Nestor","Rubio","012-345-6789","");
         });
     }
     @Test
     public void creationSucceedsWithBlankFirstName(){
         assertDoesNotThrow(()->{
-            ContactItem c = new ContactItem("","Rubio","0123456789","neee@nope.net");
+            ContactItem c = new ContactItem("","Rubio","012-345-6789","neee@nope.net");
         });
     }
     @Test
     public void creationSucceedsWithBlankLastName(){
         assertDoesNotThrow(()->{
-            ContactItem c = new ContactItem("Nestor","","0123456789","neee@nope.net");
+            ContactItem c = new ContactItem("Nestor","","012-345-6789","neee@nope.net");
         });
     }
     @Test
@@ -36,55 +51,55 @@ class ContactItemTest {
     @Test
     public void creationSucceedsWithNonBlankValues(){
         assertDoesNotThrow(()->{
-            ContactItem c = new ContactItem("Nestor","Rubio","0123456789","neee@nope.net");
+            ContactItem c = new ContactItem("Nestor","Rubio","012-345-6789","neee@nope.net");
         });
     }
     @Test
     public void editingFailsWithAllBlankValues(){
-        ContactItem c = new ContactItem("Nestor","Rubio","0123456789","neee@nope.net");
+        ContactItem c = new ContactItem("Nestor","Rubio","012-345-6789","neee@nope.net");
         assertThrows(IllegalArgumentException.class, () -> {
             c.update("","","","");
         });
     }
     @Test
     public void editingSucceedsWithBlankEmail(){
-        ContactItem c = new ContactItem("Nestor","Rubio","0123456789","neee@nope.net");
+        ContactItem c = new ContactItem("Nestor","Rubio","012-345-6789","neee@nope.net");
         assertDoesNotThrow(()->{
-            c.update("First","Last","9876543210","");
+            c.update("First","Last","987-654-3210","");
         });
     }
     @Test
     public void editingSucceedsWithBlankFirstName(){
-        ContactItem c = new ContactItem("Nestor","Rubio","0123456789","neee@nope.net");
+        ContactItem c = new ContactItem("Nestor","Rubio","012-345-6789","neee@nope.net");
         assertDoesNotThrow(()->{
-            c.update("","Last","9876543210","nope@neee.net");
+            c.update("","Last","987-654-3210","nope@neee.net");
         });
     }
     @Test
     public void editingSucceedsWithBlankLastName(){
-        ContactItem c = new ContactItem("Nestor","Rubio","0123456789","neee@nope.net");
+        ContactItem c = new ContactItem("Nestor","Rubio","012-345-6789","neee@nope.net");
         assertDoesNotThrow(()->{
-            c.update("First","","9876543210","nope@neee.net");
+            c.update("First","","987-654-3210","nope@neee.net");
         });
     }
     @Test
     public void editingSucceedsWithBlankPhone(){
-        ContactItem c = new ContactItem("Nestor","Rubio","0123456789","neee@nope.net");
+        ContactItem c = new ContactItem("Nestor","Rubio","012-345-6789","neee@nope.net");
         assertDoesNotThrow(()->{
             c.update("First","Last","","nope@neee.net");
         });
     }
     @Test
     public void editingSucceedsWithNonBlankValues(){
-        ContactItem c = new ContactItem("Nestor","Rubio","0123456789","neee@nope.net");
+        ContactItem c = new ContactItem("Nestor","Rubio","012-345-6789","neee@nope.net");
         assertDoesNotThrow(()->{
-            c.update("First","Last","9876543210","nope@neee.net");
+            c.update("First","Last","987-654-3210","nope@neee.net");
         });
     }
     @Test
     public void testToString(){
-        ContactItem c = new ContactItem("Nestor","Rubio","0123456789","neee@nope.net");
-        assertEquals("Nestor Rubio: 0123456789: neee@nope.net", c.toString());
+        ContactItem c = new ContactItem("Nestor","Rubio","012-345-6789","neee@nope.net");
+        assertEquals("Nestor Rubio: 012-345-6789: neee@nope.net", c.toString());
     }
 
 }
--- rubio_p5/src/ContactListTest.java
+++ rubio_p5/src/ContactListTest.java
@@ -7,13 +7,13 @@ class ContactListTest {
     public void addingItemsIncreasesSize(){
         ContactList c = new ContactList();
         assertEquals(0, c.size());
-        c.addContact(new ContactItem("Nestor","Rubio","3216549870","nessssss@smail.ney"));
+        c.addContact(new ContactItem("Nestor","Rubio","321-654-9870","nessssss@smail.ney"));
         assertEquals(1, c.size());
     }
     @Test
     public void editingItemsFailsWithAllBlankValues(){
         ContactList c = new ContactList();
-        c.addContact(new ContactItem("Nestor","Rubio","3216549870","nessssss@smail.ney"));
+        c.addContact(new ContactItem("Nestor","Rubio","321-654-9870","nessssss@smail.ney"));
         assertThrows(IllegalArgumentException.class,() -> {
             c.updateContact(0,"","", "","");
         } );
@@ -21,38 +21,38 @@ class ContactListTest {
     @Test
     public void editingItemsFailsWithInvalidIndex(){
         ContactList c = new ContactList();
-        c.addContact(new ContactItem("Nestor","Rubio","3216549870", "nessssss@smail.ney"));
+        c.addContact(new ContactItem("Nestor","Rubio","321-654-9870", "nessssss@smail.ney"));
         assertThrows(IndexOutOfBoundsException.class,() -> {
-            c.updateContact(1,"Chris", "Last", "3216549870","cman@smail.ney");
+            c.updateContact(1,"Chris", "Last", "321-654-9870","cman@smail.ney");
         } );
     }
     @Test
     public void editingSucceedsWithBlankFirstName(){
         ContactList c = new ContactList();
-        c.addContact(new ContactItem("Nestor","Rubio","3216549870", "nessssss@smail.ney"));
-        c.updateContact(0,"","Last", "3216548870", "nes55s@smail.ney");
+        c.addContact(new ContactItem("Nestor","Rubio","321-654-9870", "nessssss@smail.ney"));
+        c.updateContact(0,"","Last", "321-654-8870", "nes55s@smail.ney");
         assertEquals("Nestor", c.getContactFirstName(0));
     }
     @Test
     public void editingSucceedsWithBlankLastName(){
         ContactList c = new ContactList();
-        c.addContact(new ContactItem("Nestor","Rubio","3216549870", "nessssss@smail.ney"));
-        c.updateContact(0,"First","", "3216548870", "nes55s@smail.ney");
+        c.addContact(new ContactItem("Nestor","Rubio","321-654-9870", "nessssss@smail.ney"));
+        c.updateContact(0,"First","", "321-654-8870", "nes55s@smail.ney");
         assertEquals("Rubio", c.getContactLastName(0));
     }
     @Test
     public void editingSucceedsWithBlankPhone(){
         ContactList c = new ContactList();
-        c.addContact(new ContactItem("Nestor","Rubio","3216549870", "nessssss@smail.ney"));
+        c.addContact(new ContactItem("Nestor","Rubio","321-654-9870", "nessssss@smail.ney"));
         c.updateContact(0,"First","Last", "", "nes55s@smail.ney");
         assertEquals("Last", c.getContactLastName(0));
     }
     @Test
     public void editingSucceedsWithNonBlankValues(){
         ContactList c = new ContactList();
-        c.addContact(new ContactItem("Nestor","Rubio","3216549870", "nessssss@smail.ney"));
-        c.updateContact(0,"First","Last", "0123456789", "nes55s@smail.ney");
-        assertEquals("First Last: 0123456789: nes55s@smail.ney", c.contacttoString(0));
+        c.addContact(new ContactItem("Nestor","Rubio","321-654-9870", "nessssss@smail.ney"));
+        c.updateContact(0,"First","Last", "012-345-6789", "nes55s@smail.ney");
+        assertEquals("First Last: 012-345-6789: nes55s@smail.ney", c.contacttoString(0));
     }
     @Test
     public void newListIsEmpty(){
@@ -63,7 +63,7 @@ class ContactListTest {
     public void removingItemsDecreasesSize(){
         ContactList c = new ContactList();
         assertEquals(0, c.size());
-        c.addContact(new ContactItem("Nestor","Rubio","3216549870","nessssss@smail.ney"));
+        c.addContact(new ContactItem("Nestor","Rubio","321-654-9870","nessssss@smail.ney"));
         assertEquals(1, c.size());
         c.removeContact(0);
         assertEquals(0, c.size());
@@ -71,7 +71,7 @@ class ContactListTest {
     @Test
     public void removingItemsFailsWithInvalidIndex(){
         ContactList c = new ContactList();
-        c.addContact(new ContactItem("Nestor","Rubio","3216549870","nessssss@smail.ney"));
+        c.addContact(new ContactItem("Nestor","Rubio","321-654-9870","nessssss@smail.ney"));
         assertThrows(IndexOutOfBoundsException.class, () -> {
             c.removeContact(1);
         });
